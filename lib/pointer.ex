e3dd48b2a7cbaa2c25d3f1babefe265c071125cd
Mayel <git@mayel.space>
2020-07-09 09:41:37 +0200

    change default names

--- lib/pointer.ex
+++ lib/pointer.ex
@@ -9,13 +9,13 @@ defmodule Pointers.Pointer do
 
   table =
     Application.get_env(:pointers, __MODULE__, [])
-    |> Keyword.get(:source, "pointers_pointer")
+    |> Keyword.get(:source, "mn_pointer")
 
   @primary_key {:id, ULID, autogenerate: false}
   @foreign_key_type ULID
   schema(table) do
-    belongs_to :table, Table
-    field :pointed, :any, virtual: true
+    belongs_to(:table, Table)
+    field(:pointed, :any, virtual: true)
   end
 
   def create(id \\ Pointers.ULID.generate(), table) do
@@ -23,9 +23,8 @@ defmodule Pointers.Pointer do
     Changeset.cast(%Pointer{}, %{id: id, table_id: table_id}, [:id, :table_id])
   end
 
-  def repoint(%Pointer{}=pointer, table) do
+  def repoint(%Pointer{} = pointer, table) do
     table_id = Tables.id!(table)
     Changeset.cast(pointer, %{table_id: table_id}, [:table_id])
   end
-
 end
--- lib/table.ex
+++ lib/table.ex
@@ -6,22 +6,21 @@ defmodule Pointers.Table do
 
   use Pointers.Pointable,
     otp_app: :pointers,
-    source: "pointers_table",
+    source: "mn_table",
     table_id: "P01NTERTAB1EF0RA11TAB1ES00",
     autogenerate: false
-  
+
   import Ecto.Schema
 
   @type t :: %Pointers.Table{
-    table: binary,
-    schema: atom | nil,
-    pointed: term | nil,
-  }
+          table: binary,
+          schema: atom | nil,
+          pointed: term | nil
+        }
 
   pointable_schema do
-    field :table, :string
-    field :schema, :any, virtual: true
-    field :pointed, :any, virtual: true 
+    field(:table, :string)
+    field(:schema, :any, virtual: true)
+    field(:pointed, :any, virtual: true)
   end
-  
 end
