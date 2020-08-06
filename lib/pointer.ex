5e46f61c185faa4cf16e57c10e241cee15a05d42
Mayel <git@mayel.space>
2020-08-06 14:04:57 +0200

    require Flexto

--- lib/pointer.ex
+++ lib/pointer.ex
@@ -6,6 +6,7 @@ defmodule Pointers.Pointer do
   use Ecto.Schema
   alias Ecto.Changeset
   alias Pointers.{Pointer, Table, Tables, ULID}
+  require Flexto
 
   table =
     Application.get_env(:pointers, __MODULE__, [])
